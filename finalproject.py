e127320489c4b09d159ec7d799d8397f5844fd99
nktruong1996 <nktruong1996@gmail.com>
2023-07-18 00:50:05 +0700

    first part

--- finalproject.py
+++ finalproject.py
@@ -1,34 +1,29 @@
 import numpy as np
 import pandas as pd
+import streamlit as st
+import plotly.express as px
+import matplotlib.pyplot as plt
 
-msg = "Roll a dice"
-print(msg)
-
-print(np.random.randint(1,9))
-
-for num in [1,2,3,4,5]:
-    print(num)
-
-l = range(5)
-a = np.array(l)
-print(a)
-
-data = {'name' : ["An",   "Bình", "Châu", "Nam", "Mai"],
-        'grade': [   7,      6,       5,     7,     9],
-        'class': ['10A1', '10A2', '10A3', '10B', '10C']}
-df = pd.DataFrame(data)
+# importing data
+df = pd.read_csv("py4ai-score.csv")
 print(df)
 
-import streamlit as st
+# cleaning data
+df['BONUS'].fillna(0, inplace=True)
+df['S1'].fillna(0, inplace=True)
+df['S2'].fillna(0, inplace=True)
+df['S3'].fillna(0, inplace=True)
+df['S4'].fillna(0, inplace=True)
+df['S5'].fillna(0, inplace=True)
+df['S6'].fillna(0, inplace=True)
+df['S7'].fillna(0, inplace=True)
+df['S8'].fillna(0, inplace=True)
+df['S9'].fillna(0, inplace=True)
+df['S10'].fillna(0, inplace=True)
+df['REG-MC4AI'].fillna('N', inplace=True)
+print(df.isnull().sum())
 
-st.title("Hamlet said…")
-st.text("""
-To be, or not to be, that is the question:
-Whether 'tis nobler in the mind to suffer
-The slings and arrows of outrageous fortune,
-Or to take arms against a sea of troubles
-And by opposing end them. how about now?
-""")
-
-df_new = df[df['grade'] >= 7]
-print(df_new)
+# data analysis
+# pie chart(s) thể hiện tỷ lệ học sinh
+nam_nu = px.pie(df, names='GENDER')
+nam_nu.show()
